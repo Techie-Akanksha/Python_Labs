b3c4107617e07ccca37ea09394e9189f00b6bc99
Techie-Akanksha <akankshadeshmukh.in@gmail.com>
2026-09-04 13:30:33 +0530

    Build School Management System using json file

--- task.py
+++ task.py
@@ -1,35 +1,159 @@
-questions = [
-    {
-        "question": "What is the capital of India?",
-        "options": ["Mumbai", "Delhi", "Kolkata", "Chennai"],
-        "answer": "Delhi"
-    },
-    {
-        "question": "Which language are we learning?",
-        "options": ["Python", "Java", "C++", "HTML"],
-        "answer": "Python"
-    },
-    {
-        "question": "What is 5 + 3?",
-        "options": ["6", "7", "8", "9"],
-        "answer": "8"
-    }
-]
+import json
+from pathlib import Path
+from abc import ABC, abstractmethod 
 
-score = 0
+database = "school_database.json"
+data = {"students": [], "teachers": []}
 
-for question in questions:
-    print(question["question"])
+if Path(database).exists():
+    with open(database, "r") as file:
+        content = file.read()
+        if content:
+            data = json.loads(content)
 
-    for option in question["options"]:
-        print(option)
+def save_data():
+    with open(database, "w") as file:
+        json.dump(data, file, indent=4)
 
-    user_answer = input("Your answer: ")
+class Registration(ABC):
+    @abstractmethod
+    def get_roles(self):
+        pass
 
-    if user_answer.lower() == question["answer"].lower():
-        print("Correct!")
-        score += 1
-    else:
-        print("Wrong!")
+    @abstractmethod
+    def register(self):
+        pass
 
-print("Your score:", score, "/", len(questions))
+    @abstractmethod
+    def show_details(self):
+        pass
+
+    @staticmethod
+    def validate_email(email):
+        return "@" in email and "." in email
+
+class Student(Registration):
+    def get_roles(self):
+        return "Student"
+
+    def register(self):
+        name = input("Enter student name: ")
+        age = int(input("Enter student age: "))
+        email = input("Enter student email: ")
+        if not Registration.validate_email(email):
+            print("Invalid email format.")
+            return
+        roll_no = input("Enter student roll number: ")
+
+
+        for i in data["students"]:
+            if i["roll_no"] == roll_no:
+                print("Student with this roll number already exists.")
+                return
+        
+
+        student_data = {
+            "name": name, 
+            "age": age,
+            "email": email,
+            "roll_no": roll_no,
+            "grades": {}
+        }
+        data["students"].append(student_data)
+        save_data()
+        print("Student registered successfully.")
+
+    def show_details(self):
+        roll_no = input("Enter student roll number: ")
+        for student in data["students"]:
+            if student["roll_no"] == roll_no:
+                avg = sum(student["grades"].values()) / len(student["grades"]) if student["grades"] else 0
+                print(f"Name: {student['name']}")
+                print(f"Age: {student['age']}")
+                print(f"Email: {student['email']}")
+                print(f"Roll Number: {student['roll_no']}")
+                print(f"Grades: {student['grades']}")
+                print(f"Average Grade: {avg}")
+                return
+        print("Student not found.")
+        pass
+
+    def grade(self):
+        roll_no = input("Enter student roll number:")
+        subject = input("Enter subject:")
+        grade = input("Enter grade:")
+        for i in data["students"]:
+            if i["roll_no"] == roll_no:
+                i["grades"][subject] = grade
+                save_data()
+                print("Grade added successfully.")
+                return
+        print("Student not found.")
+
+
+class Teacher(Registration):
+    def get_roles(self):
+        return "Teacher"
+
+    def register(self):
+        name = input("Enter teacher name:")
+        subject = input("Enter teacher subject:")
+        email = input("Enter teacher email:")
+        teacher_id = input("Enter teacher ID:")
+
+        if not Registration.validate_email(email):
+            print("Invalid email format.")
+            return
+
+        for i in data["teachers"]:
+            if teacher_id == i["teacher_id"]:
+                print("Teacher with this ID already exists.")
+                return
+
+        teacher_data = {
+            "name": name,
+            "subject": subject,
+            "email": email,
+            "teacher_id": teacher_id
+        }
+        data["teachers"].append(teacher_data)
+        save_data()
+        print("Teacher registered successfully.")
+
+    def show_details(self):
+        teacher_id = input("Enter teacher ID: ")
+        for teacher in data["teachers"]:
+            if teacher["teacher_id"] == teacher_id:
+                print(f"Name: {teacher['name']}")
+                print(f"Subject: {teacher['subject']}")
+                print(f"Email: {teacher['email']}")
+                print(f"Teacher ID: {teacher['teacher_id']}")
+                return
+        print("Teacher not found.")
+
+
+            
+print("press 1 to register a student")
+print("press 2 to register a teacher")
+print("press 3 to add grades")
+print("press 4 to show a student details")
+print("press 5 to show a teachers details")
+
+choice =  int(input("Enter your choice:- "))
+
+student = Student()
+teacher = Teacher()
+if choice == 1:
+    student.register()
+
+elif choice == 2:
+    teacher.register()
+
+elif choice == 3:
+    student.grade()
+
+elif choice == 4:
+    student.show_details()
+
+elif choice == 5:
+    teacher.show_details()
