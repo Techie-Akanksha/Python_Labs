8bf4c7498518101dd65d0068abd3609ee1e93a8a
Techie-Akanksha <akankshadeshmukh.in@gmail.com>
2026-09-22 19:26:53 +0530

    Learn Composition vs Inheritance some real world examples of conmposition

--- index.py
+++ index.py
@@ -73,3 +73,72 @@ class Car:
 
 # Composition = object contains/uses another object.
 # Inheritance = class extends another class.
+
+# Real software example PDF Q&A application
+
+# PDFQuestionAnswerSystem HAS-A PDFLoader
+# PDFQuestionAnswerSystem HAS-A VectorStore
+# PDFQuestionAnswerSystem HAS-A LLMService 
+# That's Composition. 
+
+class PDFLoader:
+
+    def load(self, file):
+        print("Loading PDF")
+
+
+class VectorStore:
+
+    def search(self, query):
+        print("Searching vectors")
+
+
+class QA_System:
+
+    def __init__(self):
+        self.loader = PDFLoader()
+        self.vector_store = VectorStore()
+
+    def ask_question(self, file, question):
+        self.loader.load(file)
+        self.vector_store.search(question)
+
+
+# Another very common example: Service classes
+
+# Suppose you're building an e-commerce application:
+
+# OrderService
+# │
+# ├── PaymentService
+# ├── EmailService
+# └── DatabaseService
+
+# OrderService doesn't become a PaymentService. This is Composition.
+
+class PaymentService:
+
+    def pay(self, amount):
+        print("Payment:", amount)
+
+
+class OrderService:
+
+    def __init__(self):
+        self.payment = PaymentService()
+
+    def place_order(self, amount):
+        self.payment.pay(amount)
+        print("Order placed")
+
+
+
+# Interview answer
+
+# If interviewer asks:
+
+# "Where is composition used in real applications?"
+
+# You can say:
+
+# "Composition is commonly used when one class needs to use the functionality of another class. For example, an OrderService can contain a PaymentService and an EmailService, or an AI document system can contain a PDF loader, vector store, and LLM service. These components are used by the main class rather than inherited from it."
