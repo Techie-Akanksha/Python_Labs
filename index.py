6fae6705537cc05c84aedf52ada07204218454f9
Techie-Akanksha <akankshadeshmukh.in@gmail.com>
2026-09-25 17:47:46 +0530

    Learn Map function

--- index.py
+++ index.py
@@ -1,25 +1,44 @@
-# ⭐Topic - Lambda
+# ⭐Topic - map()
 
-# syntax
-# function_name = lambda parameter: expression
+# map() ka purpose:
+# Iterable ke har element par ek function apply karna.
 
-# Lambda is basically a small anonymous function.
+numbers = [1, 2, 3, 4]
 
-# Normal function
-def square(x):
-    return x * x
+squares = list(map(lambda x: x * x, numbers))
 
-#Lambda Function
-square = lambda x: x*x
+print(squares)
 
-square(5)
-
-add = lambda a, b : a+b
-print(add(5,6))
-
+# numbers
+#  ↓
+# 1 → lambda → 1
+# 2 → lambda → 4
+# 3 → lambda → 9
+# 4 → lambda → 16
+#  ↓
+# [1, 4, 9, 16]
 
 # 🎤 Interview explanation
-# A lambda function is a small anonymous function written using the lambda keyword. It can take multiple arguments but contains a single expression whose result is returned automatically.
+# map() applies a given function to every item of an iterable and returns a map object containing the results. We often convert it to a list when we want to see or store the results as a list.
 
+# Quick challenge
+# Convert this using map() + lambda:
+numbers = [2, 4, 6, 8]
+# Expected:[4, 8, 12, 16]
 
-# When to use: Usually for short, simple operations, especially with functions like map(), filter(), and sorted().
+calculation = list(map(lambda x:x*2, numbers))
+print(calculation)
+
+# numbers = [2, 4, 6, 8]
+
+# 2 → x*2 → 4
+# 4 → x*2 → 8
+# 6 → x*2 → 12
+# 8 → x*2 → 16
+
+# → map object
+# → list()
+# → [4, 8, 12, 16]
+
+# 🎤 Interview explanation
+# map() applies a function to every element of an iterable and returns a map object containing the transformed values.
